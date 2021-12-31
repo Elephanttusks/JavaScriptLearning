a57884b6dcfd21299b11d04480ba027d3bdf342c
Elephanttusks <45775430+Elephanttusks@users.noreply.github.com>
2021-12-30 19:05:55 -0700

    Add files via upload
    some specific pictures

--- main.js
+++ main.js
@@ -9,14 +9,40 @@ let color = document.getElementById('color')
 let rainbow = ['red','orange','yellow','green','blue','purple','violet'];
 
 function change() {
+  let pigpicture = document.createElement("img");
+  pigpicture.src="PigTrotting.jpeg"
+
+  let puppypicture = document.createElement("img");  
+  puppypicture.src="CuteAdorablePuppy.jpeg"
+
+  let elephant = document.createElement("img");
+  elephant.src="HappyElephant.jpeg"
 
   let response = "pigs"
 
-  text.innerText = ("Hello, " + name.value + ", you have gone through the pigsifying machine. You are now a pig.")
+  if (name.value !== ""){ 
+    text.innerText = ("Hello, " + name.value + ", you have gone through the pigsifying machine. You are now a pig.")
 
-  /* text.innerText = (name.value) */
-  console.log("Secret message for " + name.value)
-  name.value = ""
+    /* text.innerText = (name.value) */
+    console.log("Secret message for " + name.value)
+    text.appendChild(pigpicture)
+  }
+
+  if (name.value.length === 0){
+    text.innerText = "Type in your name!"
+  }
+
+  if (name.value === "Tanya"){
+    text.innerText = "Tanya, you now have the ability to turn into your favorite animal, the elephant!"
+   text.appendChild(elephant)
+  }
+
+  if (name.value === "Joel"){
+    text.innerText = "Joel, you now have the ability to turn into a dog and get petted on your fluffy head all the time."
+   text.appendChild(puppypicture)
+  }  
+    name.value = ""
+
 }
 
 function hues() {
